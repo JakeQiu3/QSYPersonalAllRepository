cd35693e614acf064a9cb003d3cfc8a738d51ee7
JakeQiu3 <qsy118614@163.com>
2016-07-22 16:19:11 +0800

    新增仿微信的demo

--- XibEnough/XibEnough/ThirdViewController.m
+++ XibEnough/XibEnough/ThirdViewController.m
@@ -8,6 +8,14 @@
 
 #import "ThirdViewController.h"
 
+
+#warning 少 给scrollView内部的子控件添加约束：
+//两个原则：1、scrollView内部子控件的尺寸不能以scrollView的尺寸为参照
+//        2、scrollView内部的子控件的约束必须完整: 也就是说-> 子控件在水平及竖直方向上的约束要把scrollView"撑满"：-> 子控件的宽高和位置都定下来。
+
+// 原因：子控件的约束决定了container scrollview的尺寸(contentSize)
+
+//解决： 1.添加到scrollView 的子控件尺寸和位置都定死（常不用）； 2.将子控件的约束和控制器的view一致/或者直接在scrollView上添加一个view（固定高度宽度位置后），等其上子控件添加完，再去掉这个view的高度的约束，约束内容View距离底部最后一个控件View的间距
 @interface ThirdViewController ()
 
 @end
