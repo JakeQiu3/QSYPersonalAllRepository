df24dc83e9c0f5783b0d7c90951766f67ee5296b
qsyMac <qsy118614@163.com>
2016-12-23 00:41:47 +0800

    添加一些内容

---
+++ QSYPersonalRepositoryAll/暂用1/UseNavBarScrollViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  NavigationBarSliding
+//
+//  Created by US10 on 16/7/21.
+//  Copyright © 2016年 US10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NavBarScrollViewController.h"
+@interface UseNavBarScrollViewController : NavBarScrollViewController<UITableViewDataSource,UITableViewDelegate>
+
+
+
+@end
+
