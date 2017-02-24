4ee9afabf98216329b9eedb8652c591c8d3ad80f
JakeQiu3 <qsy118614@163.com>
2017-02-24 19:35:14 +0800

    添加了已经修改的webview

--- QSYPersonalRepositoryAll/WebViewJavascriptBridgeLibViewController.h
+++ QSYPersonalRepositoryAll/WebViewJavascriptBridgeLibViewController.h
@@ -5,7 +5,9 @@
  *  WebViewJavascriptBridge开源库：本质通过webview的代理拦截scheme，
  *  后注入相应的JS
  *  详见： http://www.cnblogs.com/jiang-xiao-yan/p/5345755.html
- *
+ *  http://www.jianshu.com/p/f007cbd6d788
+ *  http://blog.csdn.net/junbaozi/article/details/45536237
+ *  http://blog.csdn.net/junbaozi/article/details/45536237
  */
 
 //  Created by 邱少依 on 2017/1/13.
